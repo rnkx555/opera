ba6bb8fa939bedb764cf1788f8617e4a530ed6d4
rnkx555 <p22013872@student.newinti.edu.my>
2024-08-20 22:13:41 +0800

    Add files via upload

---
+++ Opera.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.opera;
+
+/**
+ *
+ * @author rache
+ */
+public class Opera {
+
+    public static void main(String[] args) {
+        System.out.println("peking opera");
+    }
+}
